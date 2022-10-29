4042e9d0005f4751373ae656c72d0af2d2013942
Eden <edensheiko@gmail.com>
2022-10-29 14:42:44 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/Vet_program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vet_program.dir/main.cpp.o"
+  "CMakeFiles/Vet_program.dir/main.cpp.o.d"
+  "Vet_program"
+  "Vet_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vet_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
